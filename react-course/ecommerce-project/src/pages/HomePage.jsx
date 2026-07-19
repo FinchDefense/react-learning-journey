3a5635ee3fb17e67322f4e3cd71abdc0f68e1e17
Jerry Yang <87034558+FinchDefense@users.noreply.github.com>
2026-07-19 11:17:53 -0400

    Add server proxy config for /api and /images

--- react-course/ecommerce-project/src/pages/HomePage.jsx
+++ react-course/ecommerce-project/src/pages/HomePage.jsx
@@ -8,12 +8,12 @@ export function HomePage() {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3000/api/products")
+    axios.get("/api/products")
       .then((response) => {
         setProducts(response.data);
       });
 
-    axios.get('http://localhost:3000/api/cart-items')
+    axios.get('/api/cart-items')
       .then((response) => {
         setCart(response.data);
       })
@@ -52,7 +52,7 @@ export function HomePage() {
                 <div className="product-rating-container">
                   <img
                     className="product-rating-stars"
-                    src={`images/ratings/rating-${product.rating.stars * 10}`}
+                    src={`/images/ratings/rating-${product.rating.stars * 10}.png`}
                   />
                   <div className="product-rating-count link-primary">
                     {product.rating.count}
